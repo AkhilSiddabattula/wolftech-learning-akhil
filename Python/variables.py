2aee6dd5734cd0ba0636cca54b60f097124eb285
AkhilSiddabattula <akhil.siddabattula@vamstar.io>
2022-11-11 16:30:43 +0530

    Basics of Python

---
+++ Python/variables.py
@@ -0,0 +1,21 @@
+# variables in python
+# variables are used to store data in it
+a = 5  # int
+b = 5.3  # float
+c = "Variables are used to store data"  # string
+d = True  # boolean
+
+# Assigning multiple values
+x, y, z = 19, "Vamstar", False
+print(x)
+print(y)
+print(z)
+
+# To get the data type of variable we use
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+
+# Global keyword is used to set variable global
+global n
